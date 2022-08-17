1dbcfcdfc74b2498f997db59f374701526b0e450
hyuckang <hyuckang15@gmail.com>
2022-08-17 22:57:13 +0900

    terraform count test

--- Terraform/up_and_running/ch5/main.tf
+++ Terraform/up_and_running/ch5/main.tf
@@ -7,3 +7,32 @@ provider "aws" {
     region = "us-east-2"
 }
 
+resource "aws_iam_user" "example" {
+    name = "example_user"
+}
+
+resource "aws_iam_user" "count_example" {
+    count = 3
+    name = "neo.${count.index}"
+}
+
+variable "user_names_list" {
+    description = "Create IAM users with these names"
+    type = list(string)
+    default = ["neo", "trinity", "morpheus"]
+}
+
+resource "aws_iam_user" "count_var_example" {
+    count = length(var.user_names_list)
+    name = var.user_names_list[count.index]
+}
+
+output "neo_arn" {
+    value = aws_iam_user.count_var_example[0].arn
+    description = "The ARN for user Neo"
+}
+
+output "all_arns" {
+    value = aws_iam_user.count_var_example[*].arn
+    description = "The ARNs for all users"
+}
